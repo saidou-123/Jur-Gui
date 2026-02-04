148ba5a81be0608af2c0640832fa7e870387e24a
saidou-123 <saidouba.camara@etu.ussein.edu.sn>
2026-02-04 21:33:34 +0000

    Saidou

--- lib/Eleveures/AnimalInfoRFID/AnimalInfoRFIDBluetooth.dart
+++ lib/Eleveures/AnimalInfoRFID/AnimalInfoRFIDBluetooth.dart
@@ -171,7 +171,7 @@ class _AnimalInfoRFIDPageBluetoothState extends State<AnimalInfoRFIDPageBluetoot
       setState(() => _isScanning = false);
     }
     
-    debugPrint("🛑 Scan BLE arrêté");
+    debugPrint("🛑 Scan BLE arrêtéee");
   }
 
   // =====================================================
